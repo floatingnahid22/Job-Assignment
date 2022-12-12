b06b84027491849bde3bce47636dcc207bce1b4a
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-12 14:02:33 +0600

    pagination

--- ewn-task/src/components/BasicTable.js
+++ ewn-task/src/components/BasicTable.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { COLUMNS } from "./columns";
 import axios from "axios";
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, usePagination } from "react-table";
 import "./table.css";
 
 export const BasicTable = () => {
@@ -21,14 +21,29 @@ export const BasicTable = () => {
 
   const columns = useMemo(() => COLUMNS, []);
   const myData = useMemo(() => data, [data]);
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data: myData,
-      },
-      useSortBy
-    );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    page,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    state,
+    setPageSize,
+    headerGroups,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data: myData,
+      initialState: { pageIndex: 0, pageSize: 5 },
+    },
+    useSortBy,
+    usePagination
+  );
+
+  const { pageIndex, pageSize } = state;
 
   return (
     <>
@@ -48,7 +63,7 @@ export const BasicTable = () => {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {page.map((row) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
@@ -62,6 +77,39 @@ export const BasicTable = () => {
           })}
         </tbody>
       </table>
+      <div className="footer">
+        Rows per page:
+        <select
+          className="rows_per_page"
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[5, 10, 15].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}
+            </option>
+          ))}
+        </select>
+        <span>
+          <strong>
+            {pageIndex + 1} of {data.length}
+          </strong>{" "}
+        </span>
+        <button
+          className="next_prev"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          {"<"}
+        </button>{" "}
+        <button
+          className="next_prev"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          {">"}
+        </button>{" "}
+      </div>
     </>
   );
 };
