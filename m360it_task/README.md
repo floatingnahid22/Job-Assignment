645ef2b727b46b7ce5c86e5d340273a18abcfd27
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-16 15:57:20 +0600

    initial setup

---
+++ m360it_task/README.md
@@ -0,0 +1,13 @@
+# SpaceX Launches
+
+Start Project:
+
+```sh
+    npx create-react-app my-app --template typescript
+```
+
+Install Dependencies:
+
+```sh
+    npm install antd
+```
---
+++ m360it_task/src/App.tsx
@@ -0,0 +1,13 @@
+import { DatePicker } from "antd";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Date:</p>
+      <DatePicker />
+    </div>
+  );
+};
+
+export default App;
