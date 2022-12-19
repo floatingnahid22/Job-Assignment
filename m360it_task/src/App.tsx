c39f492a0c48e242f92a4ad5cea4ac6432d1f80e
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-20 00:29:21 +0600

    specific data fetched and styled cards

--- m360it_task/src/App.tsx
+++ m360it_task/src/App.tsx
@@ -1,21 +1,26 @@
 import { useMissionsQuery } from "./services/missionsApi";
 import "./App.css";
+import { Card } from "antd";
 
 const App = () => {
   const { data, error, isLoading, isFetching, isSuccess } = useMissionsQuery();
 
   return (
     <div className="App">
-      <h1>SpaceX Launches: Mission's Name</h1>
+      <h1>SpaceX Launches</h1>
       {isLoading && <h2>...Loading</h2>}
-      {isFetching && <h2>...isFetching</h2>}
+      {isFetching && <h2>...Fetching</h2>}
       {error && <h2>Something went wrong</h2>}
       {isSuccess && (
-        <div>
-          {data?.map((mission) => {
+        <div className="data">
+          {data?.map((missions, index) => {
             return (
-              <div className="data" key={mission.mission_name}>
-                <span>{mission.mission_name}</span>
+              <div className="data" key={index}>
+                <Card title={missions.mission_name} style={{ width: 300 }}>
+                  <p>Flight Number: {missions.flight_number}</p>
+                  <p>Missions Name: {missions.mission_name}</p>
+                  <p>Launch Date Utc: {missions.launch_date_utc} </p>
+                </Card>
               </div>
             );
           })}
--- m360it_task/src/models/mission.model.ts
+++ m360it_task/src/models/mission.model.ts
@@ -1,8 +1,11 @@
 export interface Mission {
+  launches: string;
+  flight_number: string;
   mission_name: string;
+  mission_id: string;
   upcoming: string;
   launch_date_utc: string;
-  rocket: string;
+  rocket_name: string;
   launch_success: string;
-  links: string;
+  mission_patch_small: string;
 }
