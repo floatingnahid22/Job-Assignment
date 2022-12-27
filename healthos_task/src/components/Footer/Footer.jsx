d909f8adcf5b6dca3c39b70e95232dbfddde8f8b
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-28 02:53:36 +0600

    footer section done

--- healthos_task/src/components/Footer/Footer.jsx
+++ healthos_task/src/components/Footer/Footer.jsx
@@ -1,7 +1,95 @@
 import React from "react";
+import "./footer.css";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div>
+              <p className="footer__text mt-4">
+                <small>
+                  Copyright &copy; {year} Nahid Hossain. All Rights Reserved
+                </small>
+              </p>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Skin Care Products</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Men's Products</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Womens Care Products</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 ">
+                  <Link to="#">Prescription Medicines</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contacts</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                  </span>
+                  <p>West Nakhalpara, Tejgaon, Dhaka</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                  </span>
+                  <p>01963363460</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                  </span>
+                  <p>nahidhossain1184@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
