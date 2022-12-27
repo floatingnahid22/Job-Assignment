19e55a08c8b730a17fbbd6c5fb7775f7690e0d74
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-27 22:14:52 +0600

    hero section done

--- healthos_task/src/pages/Home.jsx
+++ healthos_task/src/pages/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/home.css";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending products in {year}</p>
+                <h2>Making You more healthy</h2>
+                <p>
+                  Our mission is to change the community's perspective on
+                  aspects of healthcare. Let's join us!
+                </p>
+
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="heroImage" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
