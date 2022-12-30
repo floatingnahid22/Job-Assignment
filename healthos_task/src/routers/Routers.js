0c9e26e27cd13eefa766dc2a79526a0aba564add
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-30 21:45:30 +0600

    setup for admin dashboard

--- healthos_task/src/routers/Routers.js
+++ healthos_task/src/routers/Routers.js
@@ -10,6 +10,10 @@ import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import ProtectedRoute from "./ProtectedRoute";
 
+import AddProducts from "../admin/AddProducts";
+import AllProducts from "../admin/AllProducts";
+import Dashboard from "../admin/Dashboard";
+
 const Routers = () => {
   return (
     <Routes>
@@ -18,15 +22,14 @@ const Routers = () => {
       <Route path="shop" element={<Shop />} />
       <Route path="shop/:id" element={<ProductDetails />} />
       <Route path="cart" element={<Cart />} />
-      <Route
-        path="checkout"
-        element={
-          <ProtectedRoute>
-            {" "}
-            <Checkout />{" "}
-          </ProtectedRoute>
-        }
-      />
+
+      <Route path="/*" element={<ProtectedRoute />}>
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard/all-products" element={<AllProducts />} />
+        <Route path="dashboard/add-products" element={<AddProducts />} />
+      </Route>
+
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
     </Routes>
