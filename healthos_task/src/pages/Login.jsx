d7371f30e1869f4a168502ee75b18994895171b0
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-30 18:25:20 +0600

    login and sign up form done

--- healthos_task/src/pages/Login.jsx
+++ healthos_task/src/pages/Login.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../firebase.config";
+
+import "../styles/login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      console.log(user);
+      setLoading(false);
+      toast.success("Successfully logged in");
+      navigate("/home");
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <Helmet title="Login">
+      <section>
+        <Container>
+          <Row>
+            {loading ? (
+              <Col lg="12" className="text-center">
+                <h5 className="fw-bold">Loading..</h5>
+              </Col>
+            ) : (
+              <Col lg="6" className="m-auto text-center">
+                <h3 className="fw-bold mb-2">Login</h3>
+
+                <Form className="auth__form" onSubmit={loginUser}>
+                  <FormGroup className="form__group">
+                    <input
+                      type="email"
+                      placeholder="Enter your Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup className="form__group">
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </FormGroup>
+
+                  <button type="submit" className="buy__btn auth__btn">
+                    Login
+                  </button>
+                  <p>
+                    Don't have an account?{" "}
+                    <Link to="/signup">Create an account</Link>
+                  </p>
+                </Form>
+              </Col>
+            )}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Login;
