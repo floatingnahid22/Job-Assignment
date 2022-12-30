651363e1bc6389abb81afc14b2bac113bd2c7f72
floatingnahid22 <nahid.uap.github@gmail.com>
2022-12-30 23:49:18 +0600

    admin-portion partially done

--- healthos_task/src/admin/Dashboard.jsx
+++ healthos_task/src/admin/Dashboard.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import "../styles/dashboard.css";
+
+const admin__nav = [
+  {
+    display: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    display: "All-Products",
+    path: "/dashboard/all-products",
+  },
+  {
+    display: "Orders",
+    path: "/dashboard/orders",
+  },
+  {
+    display: "Users",
+    path: "/dashboard/users",
+  },
+];
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <section className="admin__menu p-2">
+      <Container>
+        <Row>
+          <Col>
+            <div className="admin__navigation">
+              <ul className="admin__menu-list">
+                {admin__nav.map((item, index) => (
+                  <li className="admin__menu-item" key={index}>
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Dashboard;
